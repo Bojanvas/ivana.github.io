e187cf48a1f414079658feae95eb5fabf39d18cb
Bojan Vasilevski <bojan87vasilevski@gmail.com>
2019-04-27 20:32:11 +0200

    ivana updated portfolio box

--- app.js
+++ app.js
@@ -22,10 +22,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Handle 404
+app.use(function(req, res) {
+  res.sendFile('404.html', {root : __dirname + '/routes/views'});
 });
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404,''));
+// });
+
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.sendFile('index.html', {root : __dirname + '/views'});
 });
 
+/* GET home page. */
+router.get('/client:id', function(req, res, next) {
+  var fileName = req.params.id;
+  console.log(fileName);
+  res.sendFile('client.html', {root : __dirname + '/views'});
+});
+
 module.exports = router;
